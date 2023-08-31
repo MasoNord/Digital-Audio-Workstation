c30f482216d38388979b28a33305f38f236b3b84
Masonord <trotf233@gmail.com>
2023-09-01 00:41:48 +0200

    Add EQs window to get list of available equalizers

---
+++ src/eqs.window.ts
@@ -0,0 +1,50 @@
+import { FlexLayout, QPushButton, QWidget} from "@nodegui/nodegui";
+import { AbstractEQ, EQ, EQsFactory } from "./creational/abstract.factory";
+
+export class Extra extends QWidget {
+    private button?: QPushButton;
+    private rootLayout?: FlexLayout;
+    private pressed: boolean = false;
+    
+    constructor() {
+        super();
+        super.setObjectName("myroot");
+        super.setFixedSize(400, 200);
+        this.rootLayout = new FlexLayout();
+        this.button = new QPushButton();
+        super.setLayout(this.rootLayout);
+        super.setWindowTitle("EQs");
+        
+        this.button.setText("Show EQs");
+        this.button.setObjectName("button");
+        this.button.addEventListener("clicked", () => {
+            if(!this.pressed) {
+                this.show_EQs(new EQ());
+                this.pressed = true;
+            }
+        })
+
+        this.rootLayout.addWidget(this.button);
+        super.setStyleSheet(`
+            #myroot {
+                align-items: 'center';
+                justify-content: 'center';
+            }
+        `);
+
+        super.show();
+    }
+
+
+    private show_EQs(factory: EQsFactory) {
+        const filters: AbstractEQ[] = factory.creatEQs();
+        let button: QPushButton;
+        // this.rootLayout?.removeWidget(this.button!);
+        
+        for (let i = 0; i < filters.length; i++) {
+            button = new QPushButton();
+            button.setText(filters[i].getName());
+            this.rootLayout?.addWidget(button);
+        }
+    }
+}
