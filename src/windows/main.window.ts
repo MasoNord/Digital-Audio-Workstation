17e6ce25a04de15efe92bb20e1fe86dfe32100dc
Masonord <trotf233@gmail.com>
2023-09-04 23:46:04 +0200

    Add button to open footr window through the main window

--- src/windows/main.window.ts
+++ src/windows/main.window.ts
@@ -1,13 +1,16 @@
 import {QMainWindow, QPushButton, QWidget, FlexLayout} from '@nodegui/nodegui';
 import { Extra } from './eqs.window';
+import { Footer } from './footer.window';
 
 export class MainWindow extends QMainWindow{
     private static _instance: MainWindow;
     private static centralWidget: QWidget;
     
     private rootLayout?: FlexLayout;
-    private button?: QPushButton;
+    private eqsButton?: QPushButton;
+    private pluginButton?: QPushButton;
     private extraWindow?: Extra;
+    private footerWindow?: Footer;
 
     constructor() {
         super();
@@ -19,18 +22,26 @@ export class MainWindow extends QMainWindow{
         
         this.rootLayout = new FlexLayout();
         MainWindow.centralWidget = new QWidget();
-        this.button = new QPushButton();
+        this.eqsButton = new QPushButton();
+        this.pluginButton = new QPushButton();
 
         MainWindow.centralWidget.setObjectName("myroot");
         MainWindow.centralWidget.setLayout(this.rootLayout);
               
-        this.button.setText("Open eqs window")
-        this.button.setObjectName("button");
-        this.button.addEventListener("clicked", () => {
+        this.eqsButton.setText("Open eqs window")
+        this.eqsButton.setObjectName("eqsButton");
+        this.eqsButton.addEventListener("clicked", () => {
             this.create_window_eqs();
         })
 
-        this.rootLayout.addWidget(this.button);
+        this.pluginButton.setText("Open footer");
+        this.pluginButton.setObjectName("pluginButton");
+        this.pluginButton.addEventListener("clicked", () => {
+            this.create_window_plugin();
+        })
+
+        this.rootLayout.addWidget(this.eqsButton);
+        this.rootLayout.addWidget(this.pluginButton);
         super.setCentralWidget(MainWindow.centralWidget);
         super.setStyleSheet(`
             #myroot {
@@ -43,5 +54,8 @@ export class MainWindow extends QMainWindow{
     }
     private create_window_eqs() {
         this.extraWindow = new Extra();
+    }
+    private create_window_plugin() {
+        this.footerWindow = new Footer();
     }
 }
