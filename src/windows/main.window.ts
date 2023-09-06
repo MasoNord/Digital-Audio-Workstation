09e9255d3a1a593115b154674a522e1e71a3b582
Masonord <trotf233@gmail.com>
2023-09-06 10:36:54 +0200

    Add button to open converter window

--- src/windows/main.window.ts
+++ src/windows/main.window.ts
@@ -1,6 +1,7 @@
 import {QMainWindow, QPushButton, QWidget, FlexLayout} from '@nodegui/nodegui';
 import { Extra } from './eqs.window';
 import { Footer } from './footer.window';
+import { Converter } from './converter.window';
 
 export class MainWindow extends QMainWindow{
     private static _instance: MainWindow;
@@ -9,8 +10,10 @@ export class MainWindow extends QMainWindow{
     private rootLayout?: FlexLayout;
     private eqsButton?: QPushButton;
     private pluginButton?: QPushButton;
+    private converterButton?: QPushButton;
     private extraWindow?: Extra;
     private footerWindow?: Footer;
+    private converter?: Converter;
 
     constructor() {
         super();
@@ -20,28 +23,33 @@ export class MainWindow extends QMainWindow{
         }
         MainWindow._instance = this;
         
-        this.rootLayout = new FlexLayout();
         MainWindow.centralWidget = new QWidget();
+        this.rootLayout = new FlexLayout();
         this.eqsButton = new QPushButton();
         this.pluginButton = new QPushButton();
+        this.converterButton = new QPushButton();
 
         MainWindow.centralWidget.setObjectName("myroot");
         MainWindow.centralWidget.setLayout(this.rootLayout);
               
         this.eqsButton.setText("Open eqs window")
-        this.eqsButton.setObjectName("eqsButton");
         this.eqsButton.addEventListener("clicked", () => {
             this.create_window_eqs();
         })
 
         this.pluginButton.setText("Open footer");
-        this.pluginButton.setObjectName("pluginButton");
         this.pluginButton.addEventListener("clicked", () => {
             this.create_window_plugin();
         })
 
+        this.converterButton.setText("Converter");
+        this.converterButton.addEventListener('clicked', () => {
+            this.open_converte_window();
+        })
+
         this.rootLayout.addWidget(this.eqsButton);
         this.rootLayout.addWidget(this.pluginButton);
+        this.rootLayout.addWidget(this.converterButton);
         super.setCentralWidget(MainWindow.centralWidget);
         super.setStyleSheet(`
             #myroot {
@@ -57,5 +65,8 @@ export class MainWindow extends QMainWindow{
     }
     private create_window_plugin() {
         this.footerWindow = new Footer();
+    }
+    private open_converte_window() {
+        this.converter = new Converter();
     }
 }
